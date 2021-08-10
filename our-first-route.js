b2c6ee39b7333394edc70ebcfae46da30103ee92
roofimon <roof@odds.team>
2021-08-10 14:51:20 +0700

    Add create update delete

--- our-first-route.js
+++ our-first-route.js
@@ -1,4 +1,6 @@
-const allTodos = require('./schema')
+const {allTodos, addTodo, updateTodo, deleteTodo} = require('./schema')
+const { v4: uuidv4 } = require('uuid')
+
 async function routes(fastify, options) {
     const client = fastify.db.client
 
@@ -12,15 +14,12 @@ async function routes(fastify, options) {
         }
     })
 
-    fastify.post('/', { schema: addTodo }, async  (request, reply) => {
+    fastify.post('/', { schema: addTodo }, async (request, reply) => {
         const { name, important, dueDate } = request.body
-        const id = uuidv4()
-        const done = false
-        createdAt = new Date().toISOString()
         const query = {
             text: `INSERT INTO todos (id, name, "createdAt", important, "dueDate", done)
-                                VALUES($1, $2, $3, $4, $5, $6 ) RETURNING *`,
-            values: [id, name, createdAt, important, dueDate, done],
+                    VALUES($1, $2, $3, $4, $5, $6 ) RETURNING *`,
+            values: [uuidv4(), name, new Date().toISOString(), important, dueDate, false],
         }
         try {
             const { rows } = await client.query(query)
@@ -31,6 +30,38 @@ async function routes(fastify, options) {
             throw new Error(err)
         }
 
+    })
+
+    fastify.patch('/:id', { schema: updateTodo }, async function (request, reply) {
+        const id = request.params.id
+        const { important, dueDate, done } = request.body
+        const query = {
+            text: `UPDATE todos SET 
+                                important = COALESCE($1, important), 
+                                "dueDate" = COALESCE($2, "dueDate"), 
+                                done = COALESCE($3, done) 
+                                WHERE id = $4 RETURNING *`,
+            values: [important, dueDate, done, id]
+        }
+        try {
+            const { rows } = await client.query(query)
+            console.log(rows[0])
+            reply.code(204)
+        } catch (err) {
+            throw new Error(err)
+        }
+    })
+
+    fastify.delete('/:id', { schema: deleteTodo }, async function (request, reply) {
+        console.log(request.params)
+        try {
+            const { rows } = await client.query(`DELETE FROM todos WHERE id = $1 RETURNING *`,
+                [request.params.id])
+            console.log(rows[0])
+            reply.code(204)
+        } catch (err) {
+            throw new Error(err)
+        }
     })
 
 }
