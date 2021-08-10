800af9669c80424504387c1d845069eb698e2401
roofimon <roof@odds.team>
2021-08-10 14:50:51 +0700

    Add all schemes

--- schema.js
+++ schema.js
@@ -17,3 +17,51 @@ const allTodos = {
         }
     }
 }
+
+const addTodo = {
+    body: {
+        type: 'object',
+        required: ['name'],
+        properties: {
+            name: { type: 'string', },
+            dueDate: { type: 'string', format: 'date-time', nullable: true, default: null },
+            important: { type: 'boolean', default: false },
+        }
+    },
+    response: {
+        201: {
+            type: 'object',
+            properties: {
+                created: { type: 'boolean' }
+            }
+        }
+    }
+
+}
+
+const updateTodo = {
+    body: {
+        type: 'object',
+        properties: {
+            dueDate: { type: 'string', format: 'date-time' },
+            important: { type: 'boolean' },
+            done: { type: 'boolean' }
+        }
+    },
+    params: {
+        type: 'object',
+        properties: {
+            id: { type: 'string', format: 'uuid' }
+        }
+    }
+}
+
+const deleteTodo = {
+    params: {
+        type: 'object',
+        properties: {
+            id: { type: 'string', format: 'uuid' }
+        }
+    }
+}
+
